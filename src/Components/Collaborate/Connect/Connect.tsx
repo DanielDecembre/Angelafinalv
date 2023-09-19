97f3028dd122ed420996591eaf2da0bfe43edb7e
Kenton Duprey <kentonduprey@icloud.com>
2023-09-18 22:41:42 -0400

    Adding contact text and blurb

--- src/Components/Collaborate/Connect/Connect.tsx
+++ src/Components/Collaborate/Connect/Connect.tsx
@@ -5,6 +5,13 @@ export const Connect = () => {
 		<div className="connect-container">
 			<div className="header-container">
 				<h1>Get in Touch</h1>
+				<div>
+					<p>
+						Based in Silicon Valley, working remotely with global
+						clients.
+					</p>
+					<a href="tel:+15152082306">+1.515.208.2306</a>
+				</div>
 			</div>
 		</div>
 	);
