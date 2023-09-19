a48eb1674ff019d4f0b5ed5359e48d44be1f05ae
DanielDecembre <96702604+DanielDecembre@users.noreply.github.com>
2023-09-18 21:46:32 -0400

    final ver

--- src/Components/Collaborate/Connect.tsx
+++ src/Components/Collaborate/Connect.tsx
@@ -1,16 +1,22 @@
-import React from 'react'
-import "./Connect.css"
+import React from 'react';
+import './Connect.css';
+import line from '../Dash.png';
+import Image from 'next/Image';
 
 function Connect() {
   return (
     <div className="connect-container">
       <div className="header-container">
-        <h1>Get in Touch</h1>
+        <h1>
+          Get in touch <Image src={line}></Image>
+        </h1>
       </div>
-
+      <div className="info-container">
+        <p>Based in Silicon Valley, working remotely with global clients.</p>
+        <p>+1.515.208.2306</p>
       </div>
-  )
+    </div>
+  );
 }
 
-export default Connect
-
+export default Connect;
